ba6daf75d39fa2703cef9bcbf8870913b1b65f40
stan <stanislav@yarmonov.pro>
2014-01-29 18:57:39 +0200

    +Added authorship.

--- WWW/com.arachne-cms/App_Start/RouteConfig.cs
+++ WWW/com.arachne-cms/App_Start/RouteConfig.cs
@@ -27,8 +27,15 @@ namespace WWW
                 constraints: new { id = @"^\d{4}-\d{2}-\d{2}-.*$" }
             );
 
-            routes.MapRoute(name: "ContentAboutImage", url: "{language}/about/{id}/{image}", defaults: new { controller = "Content", action = "Image", category = "about", id = UrlParameter.Optional });
-            routes.MapRoute(name: "ContentAbout", url: "{language}/about/{id}", defaults: new { controller = "Content", action = "Index", category = "about", id = UrlParameter.Optional });
+            routes.MapRoute(
+                name: "Content",
+                url: "{language}/{category}/{id}",
+                defaults: new { controller = "Content", action = "Index", id = UrlParameter.Optional },
+                constraints: new { category = @"^(about|contact)$" }
+            );
+
+            //routes.MapRoute(name: "ContentAboutImage", url: "{language}/about/{id}/{image}", defaults: new { controller = "Content", action = "Image", category = "about", id = UrlParameter.Optional });
+            //routes.MapRoute(name: "ContentAbout", url: "{language}/about/{id}", defaults: new { controller = "Content", action = "Index", category = "about", id = UrlParameter.Optional });
 
             routes.MapRoute(name: "ReferenceGet", url: "reference/{*id}", defaults: new { controller = "Reference", action = "Index", id = UrlParameter.Optional });
             routes.MapRoute(name: "BlogGet", url: "blog/{*id}", defaults: new { controller = "Blog", action = "Index", id = UrlParameter.Optional });
