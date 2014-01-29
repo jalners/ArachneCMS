c5fc9107dcd2f29e1b4c82038bf83bf79a9a528a
stan <stanislav@yarmonov.pro>
2014-01-29 10:00:03 +0200

    +Added blog post idea about code simplicity.

--- WWW/com.arachne-cms/App_Start/RouteConfig.cs
+++ WWW/com.arachne-cms/App_Start/RouteConfig.cs
@@ -13,6 +13,20 @@ namespace WWW
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute(
+                name: "BlogPost",
+                url: "{language}/blog/{id}",
+                defaults: new { controller = "Blog", action = "Post", id = UrlParameter.Optional },
+                constraints: new { id = @"^\d{4}-\d{2}-\d{2}-.*$" }
+            );
+
+            routes.MapRoute(
+                name: "BlogPostImage",
+                url: "{language}/blog/{id}/{image}",
+                defaults: new { controller = "Blog", action = "Image", id = UrlParameter.Optional },
+                constraints: new { id = @"^\d{4}-\d{2}-\d{2}-.*$" }
+            );
+
             routes.MapRoute(name: "ContentAboutImage", url: "{language}/about/{id}/{image}", defaults: new { controller = "Content", action = "Image", category = "about", id = UrlParameter.Optional });
             routes.MapRoute(name: "ContentAbout", url: "{language}/about/{id}", defaults: new { controller = "Content", action = "Index", category = "about", id = UrlParameter.Optional });
 
--- WWW/com.arachne-cms/Controllers/BlogController.cs
+++ WWW/com.arachne-cms/Controllers/BlogController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Text.RegularExpressions;
+using WWW.ViewModels;
 
 namespace WWW.Controllers
 {
     public class BlogController : Controller
     {
+        private static readonly Regex REGEX_POST = new Regex(@"^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<id>.*)$");
+
         public ActionResult Index(string id)
         {
             string path = string.Concat(
@@ -18,6 +22,51 @@ namespace WWW.Controllers
             string content = System.IO.File.ReadAllText(this.Server.MapPath(path));
 
             return View("Index", (object)content);
+        }
+
+        public ActionResult Post(string language, string id)
+        {
+            PageModel model = new PageModel(this.HttpContext);
+            Match match = REGEX_POST.Match(id);
+            if (match.Success)
+            {
+                string viewName = string.Format(
+                    "~/content/blog/{0}/{1}/{2}/{3}.{4}.cshtml",
+                    match.Result("${year}"),
+                    match.Result("${month}"),
+                    match.Result("${day}"),
+                    language,
+                    match.Result("${id}")
+                );
+
+                return View(viewName, model);
+            }
+            else
+            {
+                return this.HttpNotFound();
+            }
+        }
+
+        public ActionResult Image(string language, string id, string image)
+        {
+            PageModel model = new PageModel(this.HttpContext);
+            Match match = REGEX_POST.Match(id);
+            if (match.Success)
+            {
+                string path = string.Format(
+                    "~/content/blog/{0}/{1}/{2}/{3}",
+                    match.Result("${year}"),
+                    match.Result("${month}"),
+                    match.Result("${day}"),
+                    image
+                );
+
+                return this.File(path, MimeMapping.GetMimeMapping(path));
+            }
+            else
+            {
+                return this.HttpNotFound();
+            }
         }
     }
 }
